7fa51f5a8842f906ab0978954da7126507e234a9
PolezhaievBohdan <polezhaevbogdan@gmail.com>
2026-02-05 21:54:00 +0200

    Add MSTest unit tests for MathFunctions
    Added MSTest project calcu1.Tests and unit tests for calculator math operations

---
+++ calcu1.Tests/MathFunctionsTests..cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calcu1;
+
+namespace calcu1.Tests
+{
+    [TestClass]
+    public class MathFunctionsTests
+    {
+        [TestMethod]
+        public void Add_2_3_Returns_5()
+        {
+            var mf = new MathFuctions();
+            var result = mf.calc(2, 3, "Add");
+            Assert.AreEqual(5, result, 1e-9);
+        }
+
+        [TestMethod]
+        public void Div_10_2_Returns_5()
+        {
+            var mf = new MathFuctions();
+            var result = mf.calc(10, 2, "Div");
+            Assert.AreEqual(5, result, 1e-9);
+        }
+
+        [TestMethod]
+        public void InvA_4_Returns_0_25()
+        {
+            var mf = new MathFuctions();
+            var result = mf.calc(4, 0, "InvA");
+            Assert.AreEqual(0.25, result, 1e-9);
+        }
+
+        [TestMethod]
+        public void Sin_0_Returns_0()
+        {
+            var mf = new MathFuctions();
+            var result = mf.calc(0, 0, "sin");
+            Assert.AreEqual(0, result, 1e-9);
+        }
+
+        [TestMethod]
+        public void Ctn_PiOver4_Returns_Approximately_1()
+        {
+            var mf = new MathFuctions();
+            double piOver4 = 0.7853981633974483; // π/4 (радіани)
+            var result = mf.calc(piOver4, 0, "ctn");
+            Assert.AreEqual(1.0, result, 0.01);
+        }
+    }
+}
---
+++ calcu1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("calcu1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calcu1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee184970-07af-4dfd-8bce-f37ab60dca57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
