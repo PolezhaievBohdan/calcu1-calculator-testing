2115401f6de30f77722148d535bf0dd19d8dc03b
PolezhaievBohdan <polezhaevbogdan@gmail.com>
2026-03-28 09:40:13 +0200

    Додано XML-документацію та інструкції з генерації DocFX

--- calcu1/MathFuctions.cs
+++ calcu1/MathFuctions.cs
@@ -1,109 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace calcu1
 {
-    //this class will contail all methods for all math operations
-
-    public class MathFuctions
+    /// <summary>
+    /// Надає набір статичних методів для виконання базових математичних та тригонометричних операцій.
+    /// </summary>
+    public class MathFunctions
     {
-        public static double Mem = 0;
-        double Add(double num1, double num2)
+        /// <summary>
+        /// Виконує додавання двох чисел.
+        /// </summary>
+        /// <param name="a">Перший доданок.</param>
+        /// <param name="b">Другий доданок.</param>
+        /// <returns>Сума чисел a та b.</returns>
+        public static double Add(double a, double b)
         {
-            double result = num1 + num2;
-            return result;
-        }
-        double Sub(double num1, double num2)
-        {
-            return num1 - num2;
-        }
-        double Div(double num1, double num2)
-        {
-            return num1 / num2;
-        }
-        double Mul(double num1, double num2)
-        {
-            return num1 * num2;
-        }
-        double CosMy(double num1)
-        {
-            return Math.Cos(num1);
-        }
-        double Square(double num1, double num2)
-        {
-            return num1 * num1;
-        }
-        double SinMy(double num1)
-        {
-            return Math.Sin(num1);
-        }
-        double TanMy(double num1)
-        {
-            return Math.Tan(num1);
-        }
-        double CtanMy(double num1)
-        {
-            return 1 / Math.Tan(num1);
-        }
-        double InvA(double num1)
-        {
-            if (num1 == 0) return -9999;
-            return 1.0f / num1;
-        }
-        public double calc(double num1, double num2, string Mathopr)
-        {
-            double result = 0;
-            try
-            {
-                switch (Mathopr)
-                {
-                    case "Add":
-                        result = Add(num1, num2);
-                        break;
-                    case "Sub":
-                        result = Sub(num1, num2);
-                        break;
-                    case "Div":
-                        result = Div(num1, num2);
-                        break;
-                    case "Mul":
-                        result = Mul(num1, num2);
-                        break;
-                    case "Square":
-                        result = Mul(num1, num1);
-                        break;
-                    case "cos":
-                        result = CosMy(num1);
-                        break;
-                    case "sin":
-                        result = SinMy(num1);
-                        break;
-                    case "tan":
-                        result = TanMy(num1);
-                        break;
-                    case "ctn":
-                        result = CtanMy(num1);
-                        break;
-                    case "InvA":
-                        result = InvA(num1);
-                        break;
-                    case "Other":
-                        break;
-
-                }
-            }
-            catch (Exception)
-            {
-                result = 0;
-            }
-            return result;
-
-
+            return a + b;
         }
 
+        /// <summary>
+        /// Виконує віднімання другого числа від першого.
+        /// </summary>
+        /// <param name="a">Зменшуване.</param>
+        /// <param name="b">Від'ємник.</param>
+        /// <returns>Різниця чисел a та b.</returns>
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Виконує множення двох чисел.
+        /// </summary>
+        /// <param name="a">Перший множник.</param>
+        /// <param name="b">Другий множник.</param>
+        /// <returns>Добуток чисел a та b.</returns>
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Виконує ділення першого числа на друге.
+        /// </summary>
+        /// <param name="a">Ділене.</param>
+        /// <param name="b">Дільник.</param>
+        /// <returns>Частка від ділення a на b.</returns>
+        /// <exception cref="DivideByZeroException">Викидається, якщо дільник (b) дорівнює нулю.</exception>
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Ділення на нуль неможливе.");
+
+            return a / b;
+        }
+
+        /// <summary>
+        /// Обчислює обернене значення до заданого числа (1/x).
+        /// </summary>
+        /// <param name="a">Число, для якого обчислюється обернене значення.</param>
+        /// <returns>Обернене значення (1/a).</returns>
+        /// <exception cref="DivideByZeroException">Викидається, якщо вхідне число дорівнює нулю.</exception>
+        public static double Inverse(double a)
+        {
+            if (a == 0)
+                throw new DivideByZeroException("Неможливо знайти обернене значення для нуля.");
+
+            return 1 / a;
+        }
+
+        /// <summary>
+        /// Обчислює синус кута.
+        /// </summary>
+        /// <param name="a">Кут у радіанах.</param>
+        /// <returns>Синус кута a.</returns>
+        public static double Sin(double a)
+        {
+            return Math.Sin(a);
+        }
+
+        /// <summary>
+        /// Обчислює косинус кута.
+        /// </summary>
+        /// <param name="a">Кут у радіанах.</param>
+        /// <returns>Косинус кута a.</returns>
+        public static double Cos(double a)
+        {
+            return Math.Cos(a);
+        }
+
+        /// <summary>
+        /// Обчислює тангенс кута.
+        /// </summary>
+        /// <param name="a">Кут у радіанах.</param>
+        /// <returns>Тангенс кута a.</returns>
+        public static double Tan(double a)
+        {
+            return Math.Tan(a);
+        }
+
+        /// <summary>
+        /// Обчислює котангенс кута.
+        /// </summary>
+        /// <param name="a">Кут у радіанах.</param>
+        /// <returns>Котангенс кута a.</returns>
+        public static double Ctn(double a)
+        {
+            return 1 / Math.Tan(a);
+        }
     }
 }
